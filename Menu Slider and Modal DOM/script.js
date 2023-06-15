af37ec051b02932bcc8a211c533ea9c3c90a657d
Saksham Garg <77485481+Saksham-garg@users.noreply.github.com>
2023-06-15 12:08:28 +0530

    Menu Slider And Modal

---
+++ Menu Slider and Modal DOM/script.js
@@ -0,0 +1,10 @@
+const toggle  = document.getElementById('toggle')
+const close  = document.getElementById('close')
+const open  = document.getElementById('open')
+const modal  = document.getElementById('modal')
+
+
+
+toggle.addEventListener('click',()=>{
+    
+})
