842bba4c7fa16a817b8e62122440e24276bf2931
Saksham Garg <77485481+Saksham-garg@users.noreply.github.com>
2023-06-25 00:02:03 +0530

    Added functinality to get songs using forEach and map

--- Lyrics Search App/script.js
+++ Lyrics Search App/script.js
@@ -33,7 +33,7 @@ function showData(data){
     result.innerHTML = `
     <ul class="songs">
     ${data.data.map((song)=>{
-        `
+        return `
         <li>
                 <span><strong>${song.artist.name}</strong> - ${song.title}</span>
                 <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
