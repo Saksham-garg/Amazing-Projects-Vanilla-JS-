863c62b4d753d7b7966578650793b87c51c5f302
Saksham Garg <77485481+Saksham-garg@users.noreply.github.com>
2023-06-25 21:33:44 +0530

    solved issue of word styling

--- Hangman Game/script.js
+++ Hangman Game/script.js
@@ -1,26 +1,19 @@
 const hangmanBodyParts = document.querySelectorAll('.figure-part')
-
 const wrongLetter = document.getElementsByClassName('wrong-letters')
-
 const wordEl = document.getElementById('word')
-
 const popup = document.getElementById('popup-container')
-
 const notification = document.getElementById('notification-container')
-
-
 const playBtn = document.getElementById('play-button')
-//Event Listener
-
 const finalMessage = document.getElementById('final-message')
 
+//Event Listener
 
 const wordsList = ["hello","giraffe","Programming","respite",'hubris']
 
 let correctLetters = ['s']
 let wrongLetters = []
-
 let selectedWord = wordsList[Math.floor(Math.random()* (wordsList.length))]
+
 function displayLetter(){
     wordEl.innerHTML=
     `
